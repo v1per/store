396c3a46bcd6b73d536b00d9450f6037ca6cebc5
Bartosz Chojnacki <bartosz.chojnacki@apptimia.com>
2019-08-09 18:56:44 +0200

    support for angular 5.2

--- src/ng2.ts
+++ src/ng2.ts
@@ -1,4 +1,4 @@
-import { OpaqueToken, NgModule, ModuleWithProviders } from '@angular/core';
+import { InjectionToken, NgModule, ModuleWithProviders } from '@angular/core';
 
 import { Reducer } from './reducer';
 import { Dispatcher } from './dispatcher';
@@ -6,11 +6,15 @@ import { Store } from './store';
 import { State } from './state';
 import { combineReducers } from './utils';
 
-export const INITIAL_REDUCER = new OpaqueToken('Token ngrx/store/reducer');
-export const INITIAL_STATE = new OpaqueToken('Token ngrx/store/initial-state');
+export const INITIAL_REDUCER = new InjectionToken('Token ngrx/store/reducer');
+export const INITIAL_STATE = new InjectionToken(
+  'Token ngrx/store/initial-state'
+);
 
-export const _INITIAL_REDUCER = new OpaqueToken('Token _ngrx/store/reducer');
-export const _INITIAL_STATE = new OpaqueToken('Token _ngrx/store/initial-state');
+export const _INITIAL_REDUCER = new InjectionToken('Token _ngrx/store/reducer');
+export const _INITIAL_STATE = new InjectionToken(
+  'Token _ngrx/store/initial-state'
+);
 
 export function _initialReducerFactory(reducer) {
   if (typeof reducer === 'function') {
@@ -30,13 +34,17 @@ export function _storeFactory(dispatcher, reducer, state$) {
   return new Store(dispatcher, reducer, state$);
 }
 
-export function _stateFactory(initialState: any, dispatcher: Dispatcher, reducer: Reducer) {
+export function _stateFactory(
+  initialState: any,
+  dispatcher: Dispatcher,
+  reducer: Reducer
+) {
   return new State(initialState, dispatcher, reducer);
 }
 
 export function _reducerFactory(dispatcher, reducer) {
   return new Reducer(dispatcher, reducer);
-};
+}
 
 /**
  * @deprecated, use StoreModule.provideStore instead!
@@ -44,16 +52,35 @@ export function _reducerFactory(dispatcher, reducer) {
 export function provideStore(_reducer: any, _initialState?: any): any[] {
   return [
     Dispatcher,
-    { provide: Store, useFactory: _storeFactory, deps: [Dispatcher, Reducer, State] },
-    { provide: Reducer, useFactory: _reducerFactory, deps: [Dispatcher, INITIAL_REDUCER] },
-    { provide: State, useFactory: _stateFactory, deps: [INITIAL_STATE, Dispatcher, Reducer] },
-    { provide: INITIAL_REDUCER, useFactory: _initialReducerFactory, deps: [_INITIAL_REDUCER] },
-    { provide: INITIAL_STATE, useFactory: _initialStateFactory, deps: [_INITIAL_STATE, INITIAL_REDUCER] },
+    {
+      provide: Store,
+      useFactory: _storeFactory,
+      deps: [Dispatcher, Reducer, State]
+    },
+    {
+      provide: Reducer,
+      useFactory: _reducerFactory,
+      deps: [Dispatcher, INITIAL_REDUCER]
+    },
+    {
+      provide: State,
+      useFactory: _stateFactory,
+      deps: [INITIAL_STATE, Dispatcher, Reducer]
+    },
+    {
+      provide: INITIAL_REDUCER,
+      useFactory: _initialReducerFactory,
+      deps: [_INITIAL_REDUCER]
+    },
+    {
+      provide: INITIAL_STATE,
+      useFactory: _initialStateFactory,
+      deps: [_INITIAL_STATE, INITIAL_REDUCER]
+    },
     { provide: _INITIAL_STATE, useValue: _initialState },
     { provide: _INITIAL_REDUCER, useValue: _reducer }
   ];
 }
-
 
 @NgModule({})
 export class StoreModule {
